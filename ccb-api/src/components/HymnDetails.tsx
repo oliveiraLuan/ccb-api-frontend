db81936f40471438cf834120b6b62187e40e9b8c
Luan de Oliveira <luan012012@hotmail.com>
2024-10-02 19:53:08 -0300

    feat: autor default quando vier  vazio

--- ccb-api/src/components/HymnDetails.tsx
+++ ccb-api/src/components/HymnDetails.tsx
@@ -8,7 +8,7 @@ export function HymnDetails({ hymn }){
     return <>
         <div>
             <h2>Título: {hymn.title}</h2>
-            <h3>Author: {hymn.songwriter.name}</h3>
+            <h3>Author: {hymn.songwriter.name !=  "" ? hymn.songwriter.name : "Não encontrado"}</h3>
             <p>{hymn.lyrics}</p>
         </div>
     </>
